182e9f410d46862b645516beb04e9db72057ffb5
jose <jyana22@gmail.com>
2017-01-01 22:15:41 -0500

    refactoring files

--- gulpfile.js
+++ gulpfile.js
@@ -2,66 +2,83 @@
 var gulp = require('gulp');
 
 // helpers
-var del = require('del');
-var rename = require('gulp-rename');
+var del = require('del'),
+    rename = require('gulp-rename')
+    gutil = require("gulp-util");
 
 //notify error
 var plumber = require('gulp-plumber');
 
 //browser reload
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
 //css
 var sass = require('gulp-sass');
 
 //plugins css
-var mqpacker = require('css-mqpacker');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var flexibility = require('postcss-flexibility');
-var cssnano = require('cssnano');
-var sourcemaps = require('gulp-sourcemaps');
+var mqpacker = require('css-mqpacker'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    flexibility = require('postcss-flexibility'),
+    cssnano = require('cssnano'),
+    sourcemaps = require('gulp-sourcemaps');
 
 //html
 var pug = require('gulp-pug');
 
 // favicon
-var favicons = require("gulp-favicons"),
-  gutil = require("gulp-util");
+var favicons = require("gulp-favicons");
 
-//path
+// generate humans.txt
+var humans = require('gulp-humans');
+
+// generate robots.txt
+var robots = require('gulp-robots');
+
+var dir = {
+  		src: './src',
+  		dist: './dist'
+  	}
+
 var paths = {
   css: ['./*.css', '!*.min.css'],
-  sass: './assest/css/**/*.scss',
-  pug: './assest/html/module/*.pug',
-  html: './assest/html/**/*.pug',
-  dest: './public'
+  sass: dir.src + '/css/**/*.scss',
+  pug: dir.src + '/html/module/*.pug',
+  fonts: dir.src + '/fonts',
+  img: dir.src + '/img'
 }
 
 // Compile pug to HTML
 gulp.task('pug', function() {
-
   return gulp.src(paths.pug)
     .pipe(plumber())
     .pipe(pug({
       pretty: true
     }))
-    .pipe(gulp.dest(paths.dest));
+    .pipe(gulp.dest(dir.dist));
 });
 
 // Clean files .css
 gulp.task('clean:styles', function() {
-  return del([paths.dest + '/css/style.css', paths.dest +
+  return del([dir.dist + '/css/style.css', dir.dist +
     '/css/style.min.css'
   ])
 });
+
+// clean others files
+gulp.task('move', function() {
+  return gulp
+      .src(dir.dist + "/img/*.+(xml|json)")
+      .pipe(gulp.dest(dir.dist));
+});
+
 
 /**
  * Compile sass - include plugin css
  */
 gulp.task('css', ['clean:styles'], function() {
-  return gulp.src('./assest/css/*.scss')
+  return gulp.src(paths.sass)
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass.sync({
@@ -79,11 +96,11 @@ gulp.task('css', ['clean:styles'], function() {
       flexibility()
     ]))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public/css'))
+    .pipe(gulp.dest(dir.dist + "/css"))
 });
 
 gulp.task('cssnano', ['css'], function() {
-  return gulp.src('./public/css/style.css')
+  return gulp.src(dir.dist + '/css/style.css')
     .pipe(postcss([
       cssnano({
         discardComments: {
@@ -92,27 +109,24 @@ gulp.task('cssnano', ['css'], function() {
       })
     ]))
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest(dir.dist + '/css'));
 });
 
-/**
- * Process tasks and reload browsers on file changes.
- * https://www.npmjs.com/package/browser-sync
- */
 gulp.task('watch', function() {
   browserSync.init({
     browser: ["google chrome"],
     server: {
-      baseDir: "./public"
+      baseDir: dir.dist
     }
   });
-  gulp.watch([paths.html], ['pug']);
-  gulp.watch(paths.dest + '/*.html').on('change', reload);
+  gulp.watch([dir.src + '/html/**/*.pug'], ['pug']);
+  gulp.watch(dir.dist + '/*.html').on('change', reload);
   gulp.watch(paths.sass, ['css']);
 });
 
 gulp.task("favicon", function() {
-  return gulp.src("./assest/img/logo.png").pipe(favicons({
+  return gulp
+    .src(paths.img + "/logo.png").pipe(favicons({
       appName: "My App",
       appDescription: "This is my application",
       background: "#fff",
@@ -134,11 +148,37 @@ gulp.task("favicon", function() {
       }
     }))
     .on("error", gutil.log)
-    .pipe(gulp.dest("./public/img"));
+    .pipe(gulp.dest(dir.dist + "/img"))
 });
 
 gulp.task('fonts', () => {
   gulp
-    .src('./assest/fonts/*.*')
-    .pipe(gulp.dest('./public/fonts'));
+    .src(paths.fonts + '*.*')
+    .pipe(gulp.dest(dir.dist + '/fonts'));
+});
+
+gulp.task('humans', function () {
+    gulp.src('index.html')
+        .pipe(humans({
+            team : 'Jose Luis Yana - developer - @jonico22',
+            thanks: [
+                'Node (@nodejs on Twitter)',
+                'Gulp (@gulpjs on Twitter)'
+            ],
+            site: [
+                'Standards: HTML5, CSS3',
+                'Components: jQuery, Normalize.css',
+                'Software: Atom'
+            ]
+        }))
+        .pipe(gulp.dest(dir.dist));
+});
+
+gulp.task('robots', function () {
+    gulp.src('index.html')
+        .pipe(robots({
+            useragent: '*',
+            disallow: ' '
+        }))
+        .pipe(gulp.dest(dir.dist));
 });
